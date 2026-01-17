673b1400df4f3be03031d7240e1c4d279144fbc5
aim <hybiekay2@gmail.com>
2026-01-17 19:42:23 +0100

    exception is exported

--- lib/src/error/forbidden_exception.dart
+++ lib/src/error/forbidden_exception.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flint_dart/flint_dart.dart';
 import 'package:flint_dart/src/error/base_exception.dart';
-import 'package:postgres/postgres.dart';
 
 class ForbiddenErorr extends BaseException {
   ForbiddenErorr({
--- lib/src/error/unauthenticated.dart
+++ lib/src/error/unauthenticated.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 
 import 'package:flint_dart/src/error/auth_exception.dart';
 
-import 'base_exception.dart';
-
 class Unauthenticated extends AuthException {
   Unauthenticated({super.code = HttpStatus.unauthorized});
 }
