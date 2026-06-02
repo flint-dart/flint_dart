cfeadc84177bc71bc8ee6a4ac6f20bc7fc763902
unknown <hybiekay2@gmail.com>
2026-06-02 22:45:52 +0100

    fix

--- example/lib/seeders/user_modelhj_seeder.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flint_dart/flint_dart.dart';
-
-class UserModelhjSeeder extends Seeder {
-  @override
-  Future<void> run() async {
-    // TODO: Add seed data here
-    Log.debug('UserModelhjSeeder ran successfully.');
-  }
-}
